6be16134650355b9debfb6013539fa5774f1c423
Equinox <johnmarkflameno@gmail.com>
2024-11-22 16:51:25 +0800

    changed the app name

--- App_TaskFlow/apps.py
+++ App_TaskFlow/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TaskflowProjectmanagementConfig(AppConfig):
+class App_TaskFlowConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'App_TaskFlow'
